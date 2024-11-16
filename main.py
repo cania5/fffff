fc3aa337d83285e3274bce05931532b0ee47265f
cania5 <118667411+cania5@users.noreply.github.com>
2024-11-17 00:48:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+class Stepper:
+    def __init__(self, limit):
+        self.limit = limit
+        self.current = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current >= self.limit:
+            raise StopIteration
+        value = self.current
+        self.current += 1
+        return value
